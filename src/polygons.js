6e8b8aa8faf3acaca3dc6bfbe281961e41140137
Daniel Dyba <daniel.dyba@gmail.com>
2015-11-11 20:46:56 -0800

    Modify internals of Polygons

--- src/polygons.js
+++ src/polygons.js
@@ -1,36 +1,46 @@
-var Polygon = function Polygon() {
+function Polygon() {
     var vertices = [];
-    this.vertices = function() {
-        return vertices;
-    };
+
+    return Object.freeze(Object.create(Polygon.prototype, {
+        vertices: {
+            value: function() {
+                return vertices;
+            }
+        }
+    }));
 };
 
 // copy and draw are shared by all instances of Polygon
 // We call Polygon.prototype the traits object, because it shares its
 // traits like any parent would with its children
-Polygon.prototype = {
+Polygon.prototype = Object.freeze({
     copy: function() {
         return "I copy.";
     },
     draw: function() {
         return "I draw.";
     }
-};
+});
 
 var FilledPolygon = function FilledPolygon() {
-    var fillPattern = "fill pattern";
-    this.fillPattern = function() {
-        return fillPattern;
-    };
+    var fillPattern = 'fill pattern';
+
+    return Object.freeze(Object.create(FilledPolygon.prototype, {
+        fillPattern: {
+            value: function() {
+                return fillPattern;
+            }
+        }
+    }));
 }
 
-FilledPolygon.prototype = Object.create(Polygon.prototype, {
+FilledPolygon.prototype = Object.freeze(Object.create(Polygon.prototype, {
     draw: {
         value: function() {
             return 'I draw filled.';
         }
     }
-});
+}));
 
 module.exports = {
     Polygon: Polygon,
